b4113736045a572364db60aa351a5318ce259ecb
Jeffrey Simon <jdsimon@gmail.com>
2016-05-30 15:34:44 -0400

    initial commit

---
+++ src/flux_master/endpoint/example.clj
@@ -0,0 +1,7 @@
+(ns flux-master.endpoint.example
+  (:require [compojure.core :refer :all]))
+
+(defn example-endpoint [{{db :spec} :db}]
+  (context "/example" []
+    (GET "/" []
+      "This is an example endpoint")))
---
+++ test/flux_master/endpoint/example_test.clj
@@ -0,0 +1,15 @@
+(ns flux-master.endpoint.example-test
+  (:require [com.stuartsierra.component :as component]
+            [clojure.test :refer :all]
+            [kerodon.core :refer :all]
+            [kerodon.test :refer :all]
+            [flux-master.endpoint.example :as example]))
+
+(def handler
+  (example/example-endpoint {}))
+
+(deftest smoke-test
+  (testing "example page exists"
+    (-> (session handler)
+        (visit "/example")
+        (has (status? 200) "page exists"))))
