91f76d789a506642a55a45f1540747926920e52f
Jeffrey Simon <jdsimon@gmail.com>
2016-06-05 10:20:02 -0400

    improved the valid rgb defn

--- src/flux_master/util.clj
+++ src/flux_master/util.clj
@@ -47,7 +47,7 @@
   (let [size-three? #(= (count %) 3)
         in-range? (fn [n] (and (>= n 0) (<= n 255)))
         all-vals-in-range? #(every? in-range? %)
-        valid-rgb? (every-pred size-three? all-vals-in-range?)]
+        valid-rgb? (every-pred vector? size-three? all-vals-in-range?)]
     (when (valid-rgb? rgb)
       rgb)))
 
