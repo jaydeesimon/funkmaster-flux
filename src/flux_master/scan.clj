12e0b4589d226b6619834aa2eba4ec973cb92d24
Jeffrey Simon <jdsimon@gmail.com>
2016-06-06 12:34:13 -0400

    refactor using run!

--- src/flux_master/scan.clj
+++ src/flux_master/scan.clj
@@ -2,26 +2,22 @@
   (:require [flux-master.db :as db]
             [clojure.core.async :refer [chan]]))
 
-(defn- upsert-bulbs! [db bulbs]
-  (reduce + (map (fn [{:keys [id ip] :as bulb}]
-                   (if (nil? (db/get-bulb db id))
-                     (db/insert-bulb db bulb)
-                     (db/update-bulb db {:ip ip :id id})))
-                 bulbs)))
+(defn- upsert-bulb! [db {:keys [id ip] :as bulb}]
+  (if (nil? (db/get-bulb db id))
+    (db/insert-bulb db bulb)
+    (db/update-bulb db {:ip ip :id id})))
 
-(defn- add-bulb-chans! [bulb-chans bulbs]
-  (doall (map (fn [{:keys [id]}]
-                (swap! bulb-chans
-                       (fn [bulb-chans id]
-                         (if (not (get bulb-chans id))
-                           (assoc bulb-chans id (chan))
-                           bulb-chans))
-                       id))
-              bulbs)))
+(defn- add-bulb-chan! [bulb-chans {:keys [id]}]
+  (swap! bulb-chans
+         (fn [bulb-chans id]
+           (if (not (get bulb-chans id))
+             (assoc bulb-chans id (chan))
+             bulb-chans))
+         id))
 
 (defn with-scanned-bulbs [component bulbs]
   (let [db (-> component :db :spec)
         bulb-chans (-> component :bulb-chans-comp :bulb-chans)]
-    (do (upsert-bulbs! db bulbs)
-        (add-bulb-chans! bulb-chans bulbs))))
+    (do (run! (partial upsert-bulb! db) bulbs)
+        (run! (partial add-bulb-chan! bulb-chans) bulbs))))
 
